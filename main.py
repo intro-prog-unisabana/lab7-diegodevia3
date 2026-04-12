b82c757955a6f5f6388e43d232c7686bacbff5a7
diego devia salas <diegodesa@unisabana.edu.co>
2026-04-12 16:58:14 -0500

    pppl

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-from password_manager import add_login, change_password, encrypt_passwords_in_file
+from password_manager import (
+    encrypt_passwords_in_file,
+    change_password,
+    add_login
+)
 
+filename = input("Enter the CSV file name:\n")
 
-def main() -> None:
-    """TODO: Parte 5 - programa principal interactivo."""
-    pass
+encrypt_passwords_in_file(filename)
 
+while True:
+    print("Options: (1) Change Password, (2) Add Password, (3) Quit:")
+    option = input()
+    if option == '1':
+        data = input("Enter the website and the new password:\n").split()
 
-if __name__ == "__main__":
-    main()
+        if len(data) < 2:
+            print("Input is in the wrong format!")
+            continue
+
+        website, password = data
+
+        if len(password) < 12:
+            print("Password is too short!")
+            continue
+
+        success = change_password(filename, website, password)
+
+        if not success:
+            print("Website not found! Operation failed.")
+        else:
+            print("Password changed.")
+
+    elif option == '2':
+        data = input("Enter the website, username, and password:\n").split()
+
+        if len(data) < 3:
+            print("Input is in the wrong format!")
+            continue
+
+        website, username, password = data
+
+        if len(password) < 12:
+            print("Password is too short!")
+            continue
+
+        add_login(filename, website, username, password)
+        print("Login added.")
+
+    elif option == '3':
+        break
+
+    else:
+        print("Invalid option selected!")
